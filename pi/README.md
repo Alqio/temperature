c42b0c94108a67b958cfa1e1323ee8f888a94f96
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-12-28 11:04:41 +0200

    Add pi code

---
+++ pi/README.md
@@ -0,0 +1,8 @@
+## Installation
+
+Follow the instructions from here: https://github.com/ttu/ruuvitag-sensor/blob/master/install_guide_pi.md
+
+
+requests==2.22.0
+ruuvitag_sensor
+python-dotenv
---
+++ pi/main.py
@@ -0,0 +1,43 @@
+import os
+from datetime import datetime
+from time import sleep
+from dotenv import load_dotenv
+
+import ruuvitag_sensor
+from ruuvitag_sensor.log import log
+
+from urllib.parse import quote
+import requests
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+
+load_dotenv()
+
+api_url = os.getenv("API_URL")
+sensor_macs = os.getenv("SENSOR_MACS").split(",")
+
+print(sensor_macs)
+
+timeout_in_seconds = 5
+sleep_time = 5
+
+
+def get_time():
+    return datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+
+
+while True:
+    datas = RuuviTagSensor.get_data_for_sensors(sensor_macs, timeout_in_seconds)
+
+    for key, value in datas.items():
+        time = get_time()
+        temperature = value["temperature"]
+        data = {
+            "temperature": value["temperature"],
+            "timestamp": time
+        }
+
+        print(f"{time}: {temperature}")
+
+        requests.post(f"{api_url}/temperatures", json=value)
+
+    sleep(sleep_time)
